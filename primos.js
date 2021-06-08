c9d95b562dc1c9acf4062ec97df994850beab160
GabRoca <ing.gabriel.romero@gmail.com>
2021-06-07 22:04:40 -0400

    Comenzando a configurar el bloque

---
+++ primos.js
@@ -0,0 +1,14 @@
+var primo = 100;
+
+
+for (let y = primo;)
+    for (let i = primo; i > 0; i--) {
+        let div = (primo / i);
+        let resto = (primo % i);
+
+        console.log("division de " + primo + "/" + i + " es " + div + "El resto es " + resto);
+
+        /*    if ((primo % i) == 0) {
+                console.log("El número " + i + " es primo");
+            }*/
+    }
